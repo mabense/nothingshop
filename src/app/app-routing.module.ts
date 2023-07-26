8c412786183a62f28218c71d2a3fab43f21eec52
mabense <benseworks@gmail.com>
2023-07-26 16:22:10 +0200

    Step 2: Routing - Create modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./pages/edit/edit.module').then(m => m.EditModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { 
+    path: '**', 
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
