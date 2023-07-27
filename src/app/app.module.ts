e7b6996981ceb6705332ad5877d1af2eecc51eb2
mabense <benseworks@gmail.com>
2023-07-27 18:41:44 +0200

    Removed menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+// import { MenuComponent } from './menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,8 +14,8 @@ import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    // MenuComponent,
+    AppComponent
   ],
   imports: [
     MatSidenavModule,
